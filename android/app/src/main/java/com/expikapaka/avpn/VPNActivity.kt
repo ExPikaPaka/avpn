41e47c1d54bc733dcf82c21b92be9d7eba45c4f8
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-04-28 00:41:25 +0300

    Updated main activity class 'VPNActivity'. Added VPNService start/stop
    Made code more readabile. Linked VPNService start when VPNConnection
    is successful and stop when button 'disconnect' pressed.

--- android/app/src/main/java/com/expikapaka/avpn/VPNActivity.kt
+++ android/app/src/main/java/com/expikapaka/avpn/VPNActivity.kt
@@ -7,21 +7,46 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.expikapaka.avpn.VPNConnection
+import com.expikapaka.avpn.VPNService
+import com.expikapaka.avpn.transmission.UDPConnection
 
+
+///////////// ABOUT THIS PACKAGE /////////////////////////////////////
+//  This package contains main activity. It creates user interface
+//    and other interactions.
+//
+//  In the context of C/C++. This file contains 'main' entry point.
+//
+//////////////////////////////////////////////////////////////////////
+
+
+// TODO:
+//   - Remove two buttons 'connect' and 'disconnect - use only one
+//       as a switch.
+//   - Create a better looking UI.
+//
+
+
+// Activity class (UI + interaction). Basically that's our main app
 class VPNActivity : AppCompatActivity(), VPNConnection.ConnectionCallback  {
+    // Some fields that program uses for his logic
     private val VPN_PREPARE_REQUEST = 1
     private var isConnected = false
-    private var vpnService: Intent? = null
     private lateinit var connectButton: Button
     private lateinit var disconnectButton: Button
     private lateinit var infoTextView: TextView
     private lateinit var connection: VPNConnection
     private var isVpnServiceRunning = false
 
+    // Function that executes only once, when app starts.
+    // It properly creates app.
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Initialization...
         super.onCreate(savedInstanceState)
         setContentView(R.layout.vpn_activity_layout)
 
+        // Retrieve UI elements
         connectButton = findViewById<Button>(R.id.connectButton)
         disconnectButton = findViewById<Button>(R.id.disconnectButton)
         infoTextView = findViewById<TextView>(R.id.infoTextView)
@@ -31,34 +56,88 @@ class VPNActivity : AppCompatActivity(), VPNConnection.ConnectionCallback  {
         connection = VPNConnection()
 
 
-        // Buttons listener
+        // Connect button listener
         connectButton.setOnClickListener {
             connectToServer()
         }
-        disconnectButton.setOnClickListener {
 
+        // Disconnect button listener
+        disconnectButton.setOnClickListener {
+            stopVPNService()
         }
     }
+
+
+
+    // 'Handshake' function between Client and Server. Establish connection.
     private fun connectToServer() {
         try {
+            // Server fields
             val ipEditText = findViewById<EditText>(R.id.ipEditText)
             val portEditText = findViewById<EditText>(R.id.portEditText)
+
+            // User fields
+            val loginEditText = findViewById<EditText>(R.id.loginEditText)
+            val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
 
             // Retrieving fields
             val serverIP = ipEditText.text.toString()
             val serverPort = portEditText.text.toString().toInt()
+            val login = loginEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
             // Connecting
-            connection.connectToServer(serverIP, serverPort, this)
+            connection.connectToServer(serverIP, serverPort, login, password, this)
+
+            // Updating user Info field
             updatePacketInfo("Connecting...")
         } catch (e: Exception) {
+            // Notify user about error
             updatePacketInfo("Failed to connect!")
+
+            // Print exception for debug purpose
             e.printStackTrace()
         }
     }
+
+    // Start VPN service that redirects all the traffic to the VPN server
+    private fun startVPNService() {
+        if (!isVpnServiceRunning) {
+            // Starting VPN service
+            val startIntent = Intent(this, VPNService::class.java)
+            startIntent.action = "start"
+            startService(startIntent)
+
+            isVpnServiceRunning = true
+
+            // Notify user
+            updatePacketInfo("VPN Service started.")
+        } else {
+            // Temporary. Later will use only one button that works like a switch.
+            updatePacketInfo("VPN Service is already running.")
+        }
+    }
+
+    // Stop redirecting traffic
+    private fun stopVPNService() {
+        if (isVpnServiceRunning) {
+            // Stopping service
+            val stopIntent = Intent(this, VPNService::class.java)
+            stopIntent.action = "stop"
+            startService(stopIntent)
+            isVpnServiceRunning = false
+            updatePacketInfo("VPN Service stopped.")
+        } else {
+            updatePacketInfo("VPN Service is not running.")
+        }
+    }
+
+    // Callback which starts VPN service on successful handshake between client and server
     override fun onConnectionResult(success: Boolean) {
         runOnUiThread {
             isConnected = if (success) {
                 updatePacketInfo("Connected!")
+                startVPNService()
                 true
             } else {
                 updatePacketInfo("Failed to connect!")
@@ -67,18 +146,18 @@ class VPNActivity : AppCompatActivity(), VPNConnection.ConnectionCallback  {
         }
     }
 
+    // Updates user 'info' field
     private fun updatePacketInfo(packetInfo: String) {
         infoTextView.text = packetInfo
     }
+
+    // Ask user for VPN rights
     private fun prepareVpnService() {
         try {
-            // Check if VPN is supported
             val intent = VpnService.prepare(this)
             if (intent != null) {
                 // Ask user for rights
                 startActivityForResult(intent, VPN_PREPARE_REQUEST)
-            } else {
-                // VPN is already prepared or not required
             }
         } catch (e: Exception) {
             e.printStackTrace()
