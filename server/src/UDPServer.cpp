1209daea7cb149c73d1d30aba948b0c57abcd195
ExPikaPaka <nicknameofficialacc@gmail.com>
2024-05-02 00:34:06 +0300

    Added Client-Server data exchange encryption. Added MTU. Made async R\W
    Now comunication between client and server is encrypted with AES-128
    algorithm.
    
    Separated read and write operations in separate threads:
    
    clientToServer():
      1. Server receives encrypted message from client.
      2. Server decrypts message.
      3. Server writes packet to the TUN device.
    
    serverToClient():
      1. Server receives message from TUN device.
      2. Server encryptes message with AES-128 and sharedSecret
      3. Server sends ecrypted message to the client

--- server/src/UDPServer.cpp
+++ server/src/UDPServer.cpp
@@ -38,8 +38,7 @@ bool UDPServer::start() {
 }
 
 void UDPServer::run() {
-    constexpr int bufferSize = 32767;
-    char buffer[bufferSize];
+    char buffer[MTU];
     tun = std::make_unique<TunInterface>("tun0", "10.0.0.1", "255.255.255.0");
     auth = std::make_unique<AuthManager>("res/db/userdb.txt");
     
@@ -47,7 +46,7 @@ void UDPServer::run() {
         sockaddr_in clientAddr;
         socklen_t clientLen = sizeof(clientAddr);
 
-        ssize_t bytesReceived = recvfrom(socketFd, buffer, bufferSize, 0, (sockaddr*)&clientAddr, &clientLen);
+        ssize_t bytesReceived = recvfrom(socketFd, buffer, MTU, 0, (sockaddr*)&clientAddr, &clientLen);
         if (bytesReceived == -1) {
             if (errno != EAGAIN || errno != EWOULDBLOCK) {
                 throw std::system_error(errno, std::system_category(), "Failed to receive data");
@@ -56,24 +55,28 @@ void UDPServer::run() {
         }
 
         std::string message(buffer, bytesReceived);
-
-        if (!auth->isClientAuthorized(clientAddr)) {
-            threadPool->enqueue([this, message, clientAddr, clientLen] {
-                handleGuest(message, clientAddr, clientLen);
-            });
-        } else {
-            threadPool->enqueue([this, message, clientAddr, clientLen] {
-                handleClient(message, clientAddr, clientLen);
-            });
-        }
+        handleGuest(message, clientAddr, clientLen);
     }
 }
 
 void UDPServer::handleGuest(std::string message, const sockaddr_in& clientAddr, socklen_t clientLen){
     if (auth->isDiffieHellmanPerformed(clientAddr)) {
         if (auth->authenticate(message, clientAddr)) {
-        std::string response = "OK";
-        sendto(socketFd, response.c_str(), response.length(), 0, (struct sockaddr*)&clientAddr, clientLen);
+            std::string response = "OK";
+            sendto(socketFd, response.c_str(), response.length(), 0, (struct sockaddr*)&clientAddr, clientLen);
+
+            // Start threads using lambdas
+            std::thread clientToServerThread([&]() {
+                runClientToServer(clientAddr, clientLen);
+            });
+
+            std::thread serverToClientThread([&]() {
+                runServerToClient(clientAddr, clientLen);
+            });
+
+            // Join the threads with the main thread
+            clientToServerThread.join();
+            serverToClientThread.join();
         } else {
             std::string response = "FAIL";
             sendto(socketFd, response.c_str(), response.length(), 0, (struct sockaddr*)&clientAddr, clientLen);
@@ -95,4 +98,48 @@ void UDPServer::handleClient(std::string message, const sockaddr_in& clientAddr,
             sendto(socketFd, buffer, bytesRead, 0, (struct sockaddr*)&clientAddr, clientLen);
         }
     } while (bytesRead == bufferSize);
+}
+
+
+void UDPServer::runClientToServer(const sockaddr_in& clientAddr, socklen_t clientLen) {
+    while (true) {
+        try {
+            clientToServer(clientAddr, clientLen);
+        }
+        catch(const std::exception& e) {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    
+}
+
+void UDPServer::runServerToClient(const sockaddr_in& clientAddr, socklen_t clientLen) {
+        while (true) {
+        try {
+            serverToClient(clientAddr, clientLen);
+        }
+        catch(const std::exception& e) {
+            std::cerr << e.what() << '\n';
+        }
+    }
+}
+
+void UDPServer::clientToServer(const sockaddr_in& clientAddr, socklen_t clientLen) {
+    char buffer[MTU];
+    ssize_t bytesReceived = recvfrom(socketFd, buffer, MTU, 0, (sockaddr*)&clientAddr, &clientLen);
+    if (bytesReceived > 0) {
+        std::string message(buffer, bytesReceived);
+        std::string decodedText = aes.decrypt(message, auth->GetSharedKey(clientAddr));
+        tun->write(decodedText.c_str(), decodedText.size());
+    }
+}
+
+void UDPServer::serverToClient(const sockaddr_in& clientAddr, socklen_t clientLen) {
+    char buffer[MTU];
+    ssize_t bytesRead = tun->read(buffer, MTU);
+    if (bytesRead > 0) {
+        std::string message(buffer, bytesRead);
+        std::string encodedText = aes.encrypt(message, auth->GetSharedKey(clientAddr));
+        sendto(socketFd, encodedText.c_str(), encodedText.size(), 0, (struct sockaddr*)&clientAddr, clientLen);
+    }
 }
