99fba1532e2aca33424125a06903c22e7ff05c4a
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-05-02 00:57:33 +0300

    Added communication encryption. Removed hardcoded values. Added timeout
    Now communication between server and client is secured with AES128
      algorithm.
    
    When VPNService starts, it tries to retrieve fields from Intent, such
      as 'serverIP', 'serverPort', 'localIP', 'localMask', 'sharedSecret'.
    If fields are provided and in good condition - service starts.
    Otherwiseservice stops
    
    If client sends packets, but doesn't receive - after 60 seconds
      automatically stop VPN service.

--- android/app/src/main/java/com/expikapaka/avpn/VPNService.kt
+++ android/app/src/main/java/com/expikapaka/avpn/VPNService.kt
@@ -3,6 +3,7 @@ package com.expikapaka.avpn
 import android.content.Intent
 import android.net.VpnService
 import android.os.ParcelFileDescriptor
+import com.expikapaka.avpn.encryption.AES128
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
@@ -24,7 +25,7 @@ import kotlin.concurrent.thread
 //   - Remove hardcoded server IP and port. Use 'Intent' to retrieve those fields
 //      from caller.
 //   - Remove debug code.
-//   - When TUN on server will be good, check packet data and internet connection.
+//
 //
 
 
@@ -32,18 +33,25 @@ import kotlin.concurrent.thread
 class VPNService : VpnService() {
     private lateinit var vpnThread: Thread // Service thread. Used to separate from UI thread
     private lateinit var parcelFileDescriptor: ParcelFileDescriptor // 'TUN' device
-
-    private val maxPacketSize = 1500 // MTU
-
-    private val ACTION_START = "start" // VPN Service 'start' action
-    private val ACTION_STOP = "stop" // VPN Service 'stop' action
-
-    private val cloc = Object() // Console synchronization
-
-    private var txCnt = 0 // Transmit packet count
-    private var rxCnt = 0 // Receive packet count
-
-    private lateinit var connection: UDPConnection
+    private lateinit var connection: UDPConnection // UDP connection to the server
+    private val ACTION_START = "start"       // VPN Service 'start' action
+    private val ACTION_STOP = "stop"         // VPN Service 'stop' action
+    private val cloc = Object()              // Console synchronization
+    private var txCnt = 0                    // Transmit packet count
+    private var rxCnt = 0                    // Receive packet count
+    private val maxPacketSize = 2400         // MTU
+    private var timeOut: Long = 60000        // 60 seconds. If after last TX packet we have no RX - then disconnect
+    private val routeIP = "0.0.0.0"          // Route address
+    private val routeMask = 0                // Route mask
+    private var serverIP = ""                // VPN server address
+    private var serverPort = 1234            // VPN server port
+    private var sharedSecret = ""            // Encryption string
+    private var localIP = ""                 // This device private network address
+    private var localMask = 24               // This device private network mask
+    private val DEFAULT_SERVER_PORT = 25565  // Default server port
+    private val DEFAULT_LOCAL_MASK = 24      // Default server port
+    private var lastTimeResetTimer: Long = 0 // Timer value to stop service in case timeout
+    private val aes = AES128()               // AES128 encryptor/decryptor
 
     // Create function
     override fun onCreate() {
@@ -56,7 +64,24 @@ class VPNService : VpnService() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         if(intent != null) {
             if(intent.action == ACTION_START) {
-                startVPN() // Running connection
+                // Retrieve fields from Intent
+                val serverIP = intent.getStringExtra("serverIP")
+                val serverPort = intent.getIntExtra("serverPort", DEFAULT_SERVER_PORT)
+                val localIP = intent.getStringExtra("localIP")
+                val localMask = intent.getIntExtra("localMask", DEFAULT_LOCAL_MASK)
+                val sharedSecret = intent.getStringExtra("sharedSecret")
+
+                // Check if serverIP and serverPort are not null
+                if(serverIP != null && localIP != null && sharedSecret != null) {
+                    this.serverIP = serverIP
+                    this.serverPort = serverPort
+                    this.localIP = localIP
+                    this.localMask = localMask
+                    this.sharedSecret = sharedSecret
+                    startVPN()
+                } else {
+                    stopVPN()
+                }
             } else if(intent.action == ACTION_STOP) {
                 stopVPN() // Stop service
             }
@@ -69,26 +94,22 @@ class VPNService : VpnService() {
     private fun startVPN() {
         vpnThread = Thread {
             try {
-                // Hardcoded IP server. I setup static IP for my server on the home router
-                val serverAddress = "192.168.0.120"
-                val serverPort = 25565
-
                 // Create a UDPConnection instance
                 connection = UDPConnection()
 
                 // Initialize connection
-                connection.setup(serverAddress, serverPort, 0)
+                connection.setup(serverIP, serverPort, 0)
 
                 // Protect socket from loopback
-                println("Is protected????: " + protect(connection.socket))
+                protect(connection.socket)
 
                 // Create a new VPN Builder
                 val builder = Builder()
 
                 // Set the VPN parameters. Later you need to get that information from the server
                 builder.setSession(getString(R.string.app_name))
-                    .addAddress("10.0.0.2", 24)
-                    .addRoute("0.0.0.0", 0)
+                    .addAddress(localIP, localMask)
+                    .addRoute(routeIP, routeMask)
                     .setMtu(maxPacketSize)
 
                 // Establish the VPN connection
@@ -98,6 +119,8 @@ class VPNService : VpnService() {
                 val vpnInput = FileInputStream(parcelFileDescriptor.fileDescriptor)
                 val vpnOutput = FileOutputStream(parcelFileDescriptor.fileDescriptor)
 
+                // Updating timer
+                lastTimeResetTimer = System.currentTimeMillis()
 
                 // Run client to server communication
                 thread (start = true) {
@@ -136,14 +159,18 @@ class VPNService : VpnService() {
 
     // Reads local TUN device and sends packet to server via connection
     private fun clientToServer(vpnInputStream: FileInputStream, serverConnection: IConnection) {
-        var readBuffer = ByteArray(maxPacketSize)
+        // Check timeout
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastTimeResetTimer > timeOut) {
+            stopVPN()
+        }
 
+        // Read form Us and send to Server
+        var readBuffer = ByteArray(maxPacketSize) // Packet buffer
+        // Read from our device
         val numberOfBytesReceived = vpnInputStream.read(readBuffer)
 
-        if (numberOfBytesReceived == -1) {
-            throw IOException()
-        }
-        if (numberOfBytesReceived == 0) {
+        if (numberOfBytesReceived <= 0) {
             return
         }
 
@@ -151,14 +178,17 @@ class VPNService : VpnService() {
         readBuffer = readBuffer.dropLast(readBuffer.size - numberOfBytesReceived).toByteArray()
 
         // Output (DEBUG)
-        synchronized(cloc) {
-            println("\n\n\nTX: $txCnt\nRead packet: " + numberOfBytesReceived + " Bytes\n")
-            displayPacketInfo(readBuffer)
-            displayPacketData(readBuffer)
-        }
+        //synchronized(cloc) {
+        //    println("\n\n\nTX: $txCnt\nRead packet: " + numberOfBytesReceived + " Bytes\n")
+        //    displayPacketInfo(readBuffer)
+        //    displayPacketData(readBuffer)
+        //}
+
+        // Encrypting
+        val encrypted = aes.encrypt(readBuffer, sharedSecret.toByteArray())
 
         // Sending packet
-        serverConnection.write(readBuffer)
+        serverConnection.write(encrypted)
         txCnt++ // Increment transmit count
     }
 
@@ -179,15 +209,19 @@ class VPNService : VpnService() {
     // Receive packets from server and write to local TUN device
     private fun serverToClient(vpnOutputStream: FileOutputStream, serverConnection: IConnection) {
         val messageData = serverConnection.read(maxPacketSize)
+        lastTimeResetTimer = System.currentTimeMillis() // Reset timer
         rxCnt++ // Increment receive count
 
         // Output (DEBUG)
-        synchronized(cloc) {
-            println("\n\nRX: $rxCnt\nGot packet from server:\n")
-            displayPacketInfo(messageData)
-            displayPacketData(messageData)
-        }
-        vpnOutputStream.write(messageData)
+        //synchronized(cloc) {
+        //    println("\n\nRX: $rxCnt\nGot packet from server:\n")
+        //    displayPacketInfo(messageData)
+        //    displayPacketData(messageData)
+        //}
+
+        // Decrypting
+        val decrypted = aes.decrypt(messageData, sharedSecret.toByteArray())
+        vpnOutputStream.write(decrypted)
     }
 
     // Stop service via closing connection
